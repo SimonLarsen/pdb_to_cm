05b3ccb102b4664e985f4c253c2eed8361862fda
Simon Larsen <simonhffh@gmail.com>
2017-05-23 15:10:16 +0200

    Added support for extracting specific chains

--- pdb_to_cm.py
+++ pdb_to_cm.py
@@ -1,6 +1,7 @@
 import sys
 import math
 import argparse
+import re
 from itertools import combinations
 
 
@@ -11,13 +12,16 @@ def dist(p1, p2):
     return math.sqrt(dx**2 + dy**2 + dz**2)
 
 
-def read_atoms(file):
+def read_atoms(file, chain="."):
+    pattern = re.compile(chain)
+
     atoms = []
     for line in file:
         line = line.strip()
         if line.startswith("ATOM"):
             type = line[12:16].strip()
-            if type == "CA":
+            chain = line[21:22]
+            if type == "CA" and re.match(pattern, chain):
                 x = float(line[30:38].strip())
                 y = float(line[38:46].strip())
                 z = float(line[46:54].strip())
@@ -39,8 +43,8 @@ def write_output(contacts, file):
         file.write("\t".join(map(str, c))+"\n")
 
 
-def pdb_to_cm(file, threshold):
-    atoms = read_atoms(file)
+def pdb_to_cm(file, threshold, chain="."):
+    atoms = read_atoms(file, chain)
     return compute_contacts(atoms, threshold)
 
 
@@ -48,10 +52,11 @@ def main():
     parser = argparse.ArgumentParser(description="Computes protein contact map from PDB file.")
     parser.add_argument("pdb_file", type=str, help="PDB input file.")
     parser.add_argument("output_file", type=str, help="File to write contact map to.")
+    parser.add_argument("-c", "--chain", type=str, required=False, default=".", help="Chain ID to use. Supports regular expression.")
     parser.add_argument("-t", "--threshold", type=float, required=False, default=7.5, help="Contact distance threshold in angstrom.")
     args = parser.parse_args()
 
-    contacts = pdb_to_cm(open(args.pdb_file, "r"), args.threshold)
+    contacts = pdb_to_cm(open(args.pdb_file, "r"), args.threshold, args.chain)
     write_output(contacts, open(args.output_file, "w"))
 
 if __name__ == '__main__':
