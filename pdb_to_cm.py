870ad8097a28e8159c1ac4c0fedbf8c65c15b0e2
Simon Larsen <simonhffh@gmail.com>
2017-05-23 17:26:20 +0200

    Added model selection argument.

--- pdb_to_cm.py
+++ pdb_to_cm.py
@@ -12,13 +12,14 @@ def dist(p1, p2):
     return math.sqrt(dx**2 + dy**2 + dz**2)
 
 
-def read_atoms(file, chain="."):
+def read_atoms(file, chain=".", model=1):
     pattern = re.compile(chain)
 
+    current_model = model
     atoms = []
     for line in file:
         line = line.strip()
-        if line.startswith("ATOM"):
+        if line.startswith("ATOM") and current_model == model:
             type = line[12:16].strip()
             chain = line[21:22]
             if type == "CA" and re.match(pattern, chain):
@@ -26,6 +27,8 @@ def read_atoms(file, chain="."):
                 y = float(line[38:46].strip())
                 z = float(line[46:54].strip())
                 atoms.append((x, y, z))
+        elif line.startswith("MODEL"):
+            current_model = int(line[10:14].strip())
     return atoms
 
 
@@ -43,8 +46,8 @@ def write_output(contacts, file):
         file.write("\t".join(map(str, c))+"\n")
 
 
-def pdb_to_cm(file, threshold, chain="."):
-    atoms = read_atoms(file, chain)
+def pdb_to_cm(file, threshold, chain=".", model=1):
+    atoms = read_atoms(file, chain, model)
     return compute_contacts(atoms, threshold)
 
 
@@ -52,11 +55,12 @@ def main():
     parser = argparse.ArgumentParser(description="Computes protein contact map from PDB file.")
     parser.add_argument("pdb_file", type=str, help="PDB input file.")
     parser.add_argument("output_file", type=str, help="File to write contact map to.")
-    parser.add_argument("-c", "--chain", type=str, required=False, default=".", help="Chain ID to use. Supports regular expression.")
     parser.add_argument("-t", "--threshold", type=float, required=False, default=7.5, help="Contact distance threshold in angstrom.")
+    parser.add_argument("-c", "--chain", type=str, required=False, default=".", help="Chain ID to use. Supports regular expression.")
+    parser.add_argument("-m", "--model", type=int, required=False, default=1, help="Model ID to use.")
     args = parser.parse_args()
 
-    contacts = pdb_to_cm(open(args.pdb_file, "r"), args.threshold, args.chain)
+    contacts = pdb_to_cm(open(args.pdb_file, "r"), args.threshold, args.chain, args.model)
     write_output(contacts, open(args.output_file, "w"))
 
 if __name__ == '__main__':
